2ee7d176e536f35ca8d8efacfe1f9ec1487b856b
nurlan <baitassovnurlan1507@gmail.com>
2024-07-24 16:13:06 +0500

    feat: redux api handlers

--- src/store/winners/index.ts
+++ src/store/winners/index.ts
@@ -2,6 +2,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { Winner } from './types';
 import { SliceState } from '../types';
+import { getWinnersAsync, updateWinnerAsync, createWinnerAsync } from './api';
+import { isApiError } from '../../utils/baseApi';
 
 interface WinnersSliceState extends SliceState<Winner[]|undefined> {
   currentPage:number,
@@ -20,6 +22,51 @@ const WinnerSlice = createSlice({
   initialState,
   reducers: {
 
+  },
+  extraReducers(builder) {
+    builder.addCase(getWinnersAsync.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(getWinnersAsync.rejected, (state, action) => {
+      state.error = action.payload;
+      state.isLoading = false;
+    });
+    builder.addCase(getWinnersAsync.fulfilled, (state, action) => {
+      state.isLoading = false;
+      state.data = action.payload.winners;
+      state.totalAmount = action.payload.totalCount;
+    });
+    builder.addCase(createWinnerAsync.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(createWinnerAsync.rejected, (state, action) => {
+      if (isApiError(action.payload)) {
+        state.isLoading = false;
+        state.error = action.payload.message;
+      }
+    });
+    builder.addCase(createWinnerAsync.fulfilled, (state, action) => {
+      if (state.data) {
+        state.data.push(action.payload);
+      }
+    });
+    builder.addCase(updateWinnerAsync.pending, (state, action) => {
+      state.isLoading = true;
+    });
+    builder.addCase(updateWinnerAsync.rejected, (state, action) => {
+      if (isApiError(action.payload)) {
+        state.isLoading = false;
+        state.error = action.payload.message;
+      }
+    });
+    builder.addCase(updateWinnerAsync.fulfilled, (state, action) => {
+      if (state.data) {
+        const index = state.data.findIndex((winner) => winner.id === action.payload?.id);
+        if (index >= 0) {
+          state.data[index] = action.payload!;
+        }
+      }
+    });
   },
 
 });
